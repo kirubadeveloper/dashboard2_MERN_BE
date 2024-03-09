07b9383b9951c3fe88a6d67363e022da7e7d9974
Kiruba97 <kirubaerd1997@gmail.com>
2024-03-09 08:21:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,78 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import kpiRoutes from "./routes/kpi.js";
+import productRoutes from "./routes/product.js";
+import transactionRoutes from "./routes/transaction.js";
+import KPI from "./models/KPI.js";
+import Product from "./models/Product.js";
+import Transaction from "./models/Transaction.js";
+import { kpis, products, transactions } from "./data/data.js";
+
+// Configurations
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// Routes
+app.use("/kpi", kpiRoutes);
+app.use("/product", productRoutes);
+app.use("/transaction", transactionRoutes);
+
+// Mongoose Setup
+
+// const PORT = process.env.PORT || 9000;
+// const MONGO_URI =
+//   process.env.MONGO_URL ||
+//   "mongodb+srv://kirubaerd1997:mongo1997@mycluster.zkvwqlt.mongodb.net/";
+
+// async function connectToMongoDB() {
+//   try {
+//     await mongoose.connect(MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log("Connected to MongoDB");
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB:", error);
+//   }
+// }
+
+// connectToMongoDB();
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(
+    "mongodb+srv://kiruba:eQzBgLIiLhvM4vGw@streamingapps.xbvbb.mongodb.net/",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(async () => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    //  For adding data one time only
+    // await mongoose.connection.db.dropDatabase();
+    // KPI.insertMany(kpis);
+    // Product.insertMany(products);
+    Transaction.insertMany(transactions);
+
+  })
+  .catch((error) => console.log(`${error} did not connect`));
